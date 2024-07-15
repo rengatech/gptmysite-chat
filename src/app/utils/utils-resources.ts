23ccaad1609fb93ca53cc75be55e7540fc6154c3
nouman-shah <shahnoman240@gmail.com>
2024-07-15 11:05:28 +0500

    first commit

---
+++ src/app/utils/utils-resources.ts
@@ -0,0 +1,55 @@
+export const BRAND_BASE_INFO: { [key: string]: string | boolean } = {
+  COMPANY_NAME: "GPT",
+  BRAND_NAME: "GPT",
+  COMPANY_SITE_NAME: "GPT.com",
+  COMPANY_SITE_URL: "https://www.tiledesk.com",
+  CONTACT_US_EMAIL: "support@tiledesk.com",
+  FAVICON: "https://i.ibb.co/cD5hS42/logo-short-0.png",
+  META_TITLE: "GPT - Open Source Live Chat",
+  DOCS: true,
+  LOGOUT_ENABLED: false,
+};
+
+export var LOGOS_ITEMS: {
+  [key: string]: { label: string | boolean; icon: string };
+} = {
+  COMPANY_LOGO: {
+    label: BRAND_BASE_INFO.COMPANY_NAME,
+    icon: "assets/logos/tiledesk_logo.svg",
+  },
+  COMPANY_LOGO_NO_TEXT: {
+    label: BRAND_BASE_INFO.COMPANY_NAME,
+    icon: "assets/logos/tiledesk_logo_no_text.svg",
+  },
+  BASE_LOGO: {
+    label: BRAND_BASE_INFO.BRAND_NAME,
+    icon: "assets/logos/tiledesk_logo.svg",
+  },
+  BASE_LOGO_NO_TEXT: {
+    label: BRAND_BASE_INFO.BRAND_NAME,
+    icon: "assets/logos/tiledesk_logo_no_text.svg",
+  },
+  BASE_LOGO_WHITE: {
+    label: BRAND_BASE_INFO.BRAND_NAME,
+    icon: '"assets/logos/tiledesk-logo_new_white.svg',
+  },
+  BASE_LOGO_WHITE_NO_TEXT: {
+    label: BRAND_BASE_INFO.BRAND_NAME,
+    icon: '"assets/logos/tiledesk-logo_new_white.svg',
+  },
+  BASE_LOGO_GRAY: {
+    label: BRAND_BASE_INFO.BRAND_NAME,
+    icon: "https://i.ibb.co/SBhhjDp/tiledesk-logo-new-white.png",
+  },
+};
+
+export const MEDIA: {
+  [key: string]: { src: string; text: string; description: string };
+} = {
+  RULES: {
+    src: "https://www.youtube.com/embed/p0ux-86Y4_I",
+    text: "CDSSplashScreen.YouHaveNoRules",
+    description: "CDSSplashScreen.LearnAboutAI",
+  },
+  GLOBALS: { src: "", text: "CDSGlobals.NoGlobalVariables", description: "" },
+};
